f8e41ab9971536fa0545fae3706843db3cdd2696
Jirka Kremser <jiri.kremser@gmail.com>
2021-12-01 15:40:20 +0000

    Another chunk of animations
    Signed-off-by: Jirka Kremser <jiri.kremser@gmail.com>

--- k8gb.py
+++ k8gb.py
@@ -17,11 +17,14 @@ red = "ffc1b6"
 # revealjs plugin https://github.com/RickDW/manim-revealjs
 
 class FailOver(MovingCameraScene):
-    config = {
+    # color scheme: https://www.reddit.com/r/manim/comments/dzxoen/predefined_color_scheme/
+    cfg = {
         "font": "Noto Sans",
         "code_font": "Arial Rounded MT Bold",
         "font_color": BLACK,
         "boxes_color": BLACK,
+        "dns_color": RED_E,
+        "http_color": BLUE,
         "color": BLACK,
     }
 
@@ -29,23 +32,24 @@ class FailOver(MovingCameraScene):
         self.camera.background_color = WHITE
         self.pods()
         # self.display_images()
+        # self.speech_bubble()
 
     def pods(self):
         self.camera.frame.save_state()
-        k8s1 = Rectangle(width=8.0, height=5.0, color=FailOver.config["boxes_color"])
-        k8s_text1 = Text("Kubernetes", color=FailOver.config["font_color"], font_size=23, font=FailOver.config["font"])
+        k8s1 = Rectangle(width=8.0, height=5.0, color=self.cfg["boxes_color"])
+        k8s_text1 = Text("Kubernetes", color=self.cfg["font_color"], font_size=23, font=self.cfg["font"])
         k8s_text1.next_to(k8s1, UP)
 
         dep1 = RoundedRectangle(width=3.0, height=2.7, stroke_width=1.0, corner_radius=0.5).set_stroke(GRAY, opacity=0.5).shift(UP*0.1+RIGHT*0.1)
-        dep_text1 = Text("App Deployment (1 pod)", color=FailOver.config["font_color"], font_size=20, font=FailOver.config["font"])
+        dep_text1 = Text("App Deployment (1 pod)", color=self.cfg["font_color"], font_size=20, font=self.cfg["font"])
         dep_text1.next_to(dep1, UP)
-        dep_text12 = Text("App Deployment (2 pods)", color=FailOver.config["font_color"], font_size=20, font=FailOver.config["font"])
+        dep_text12 = Text("App Deployment (2 pods)", color=self.cfg["font_color"], font_size=20, font=self.cfg["font"])
         dep_text12.next_to(dep1, UP)
-        pod11 = Rectangle(width=2.0, height=1.5, color=FailOver.config["boxes_color"], fill_color=WHITE, fill_opacity = 1)
-        pod_text11 = Text("</>", color=BLUE, font_size=35,font=FailOver.config["code_font"], weight=BOLD)
+        pod11 = Rectangle(width=2.0, height=1.5, color=self.cfg["boxes_color"], fill_color=WHITE, fill_opacity = 1)
+        pod_text11 = Text("</>", color=self.cfg["http_color"], font_size=35,font=self.cfg["code_font"], weight=BOLD)
         pod_text11.move_to(pod11)
-        pod12 = Rectangle(width=2.0, height=1.5, color=FailOver.config["boxes_color"], fill_color=WHITE, fill_opacity = 1)
-        pod_text12 = Text("</>", color=BLUE, font_size=35, font=FailOver.config["code_font"], weight=BOLD)
+        pod12 = Rectangle(width=2.0, height=1.5, color=self.cfg["boxes_color"], fill_color=WHITE, fill_opacity = 1)
+        pod_text12 = Text("</>", color=self.cfg["http_color"], font_size=35, font=self.cfg["code_font"], weight=BOLD)
         pod_text12.move_to(pod12)
         deployment1 = Group(dep1, pod11, pod_text11)
         pod2group = Group(pod12, pod_text12)
@@ -62,7 +66,7 @@ class FailOver(MovingCameraScene):
         happy_tux.set_height(1.3)
         happy_tux.to_edge(LEFT)
         self.play(FadeIn(happy_tux))
-        dot1 = Dot(color=BLUE)
+        dot1 = Dot(color=self.cfg["http_color"])
         dot1.move_to(happy_tux.get_edge_center(RIGHT))
         dot3 = dot1.copy()
         dot2 = dot1.copy().move_to(pod11)
@@ -97,18 +101,18 @@ class FailOver(MovingCameraScene):
         self.play(self.camera.frame.animate.scale(2))
         self.wait(1)
 
-        k8s2 = Rectangle(width=8.0, height=5.0, color=FailOver.config["boxes_color"])
-        k8s_text2 = Text("Kubernetes", color=FailOver.config["font_color"], font_size=23, font=FailOver.config["font"])
+        k8s2 = Rectangle(width=8.0, height=5.0, color=self.cfg["boxes_color"])
+        k8s_text2 = Text("Kubernetes", color=self.cfg["font_color"], font_size=23, font=self.cfg["font"])
         k8s_text2.next_to(k8s2, UP)
 
         dep2 = RoundedRectangle(width=3.0, height=2.7, stroke_width=1.0, corner_radius=0.5).set_stroke(GRAY, opacity=0.5).shift(UP*0.1+RIGHT*0.1)
-        dep_text2 = Text("App Deployment (2 pods)", color=FailOver.config["font_color"], font_size=20, font=FailOver.config["font"])
+        dep_text2 = Text("App Deployment (2 pods)", color=self.cfg["font_color"], font_size=20, font=self.cfg["font"])
         dep_text2.next_to(dep2, UP)
 
-        pod21 = Rectangle(width=2.0, height=1.5, color=FailOver.config["boxes_color"], fill_color=WHITE, fill_opacity = 1)
-        pod22 = Rectangle(width=2.0, height=1.5, color=FailOver.config["boxes_color"], fill_color=WHITE, fill_opacity = 1)
+        pod21 = Rectangle(width=2.0, height=1.5, color=self.cfg["boxes_color"], fill_color=WHITE, fill_opacity = 1)
+        pod22 = Rectangle(width=2.0, height=1.5, color=self.cfg["boxes_color"], fill_color=WHITE, fill_opacity = 1)
         pod22.shift(RIGHT*0.2).shift(UP*0.2).set_z(1)
-        pod_text22 = Text("</>", color=BLUE, font_size=35, font=FailOver.config["code_font"], weight=BOLD)
+        pod_text22 = Text("</>", color=self.cfg["http_color"], font_size=35, font=self.cfg["code_font"], weight=BOLD)
         pod_text22.move_to(pod22)
 
         cl1 = Group(k8s1,k8s_text1,dep1, dep_text12, pod11, pod12, pod_text12)
@@ -116,8 +120,8 @@ class FailOver(MovingCameraScene):
         self.add(cl2)
         self.remove(pod_text11)
         self.play(cl2.animate.move_to(RIGHT*4.6 + UP), cl1.animate.move_to(LEFT* 4.6 + UP))
-        k8s_text12 = Text("Cluster 1 (eu)", color=FailOver.config["font_color"], font_size=23, font=FailOver.config["font"])
-        k8s_text22 = Text("Cluster 2 (us)", color=FailOver.config["font_color"], font_size=23, font=FailOver.config["font"])
+        k8s_text12 = Text("Cluster 1 (eu)", color=self.cfg["font_color"], font_size=23, font=self.cfg["font"])
+        k8s_text22 = Text("Cluster 2 (us)", color=self.cfg["font_color"], font_size=23, font=self.cfg["font"])
         k8s_text12.next_to(k8s1, UP)
         k8s_text22.next_to(k8s2, UP)
         self.play(TransformMatchingShapes(k8s_text1, k8s_text12), TransformMatchingShapes(k8s_text2, k8s_text22))
@@ -138,23 +142,37 @@ class FailOver(MovingCameraScene):
         self.play(FadeIn(route))
 
         self.say_scaled("Setup load balancing strategy")
-        k8s_text13 = Text("Primary Cluster 1 (eu)", color=FailOver.config["font_color"], font_size=23, font=FailOver.config["font"])
-        k8s_text23 = Text("Secondary Cluster 2 (us)", color=FailOver.config["font_color"], font_size=23, font=FailOver.config["font"])
+        k8s_text13 = Text("Primary Cluster 1 (eu)", color=self.cfg["font_color"], font_size=23, font=self.cfg["font"])
+        k8s_text23 = Text("Secondary Cluster 2 (us)", color=self.cfg["font_color"], font_size=23, font=self.cfg["font"])
         k8s_text13.next_to(k8s1, UP)
         k8s_text23.next_to(k8s2, UP)
         self.play(TransformMatchingShapes(k8s_text12, k8s_text13), TransformMatchingShapes(k8s_text22, k8s_text23))
-        # route_bubble = SpeechBubble()
+        route_bubble = self.speech_bubble().scale(0.9).next_to(route, direction=RIGHT, buff=0)
+        self.play(Create(route_bubble))
+        dns_bubble_title = Text("app.example.com", color=self.cfg["dns_color"], font_size=25, font=self.cfg["font"])
+        dns_bubble_title.next_to(route_bubble, direction=UP, buff=0.1).shift(RIGHT*0.3)
+        self.play(Write(dns_bubble_title))
+        dns_bubble_r1 = Text("192.168.0.1 (node 1 @ eu)", color=self.cfg["dns_color"], font_size=18, font=self.cfg["font"])
+        dns_bubble_r2 = Text("192.168.0.2 (node 2 @ eu)", color=self.cfg["dns_color"], font_size=18, font=self.cfg["font"])
+        dns_bubble_r1.next_to(dns_bubble_title, direction=DOWN, buff=0.4)
+        dns_bubble_r2.next_to(dns_bubble_r1, direction=DOWN, buff=0.2)
+        ttl_text = Text("(TTL = 30sec)", color=self.cfg["dns_color"], font_size=25, font=self.cfg["font"])
+        ttl_text.next_to(route_bubble, direction=RIGHT)
+        self.play(Write(dns_bubble_r1), run_time=0.5)
+        self.play(Write(dns_bubble_r2), run_time=0.5)
+        self.play(Write(ttl_text), run_time=1)
 
         self.say_scaled("Let's look how failover strategy works")
         # show tux again
         happy_tux2 = ImageMobject(fr"images/happy_tux.png")
         happy_tux2.scale(0.7)
         happy_tux2.move_to(self.camera.frame.get_left()).shift(RIGHT*1.5+DOWN*3)
+        happy_tux2_orig = happy_tux2.copy()
 
-        dot_dns = Dot(color=RED, radius=DEFAULT_DOT_RADIUS*2).move_to(self.camera.frame.get_left()).shift(RIGHT*1.5+UP*1)
-        dot_http = Dot(color=BLUE, radius=DEFAULT_DOT_RADIUS*2).next_to(dot_dns, direction=DOWN, buff=0.5)
-        dot_dns_text = Text("dns resolution", font_size=24, color=RED, font=FailOver.config["font"]).next_to(dot_dns, direction=RIGHT, buff=0.7)
-        dot_http_text = Text("HTTP request", font_size=24, color=BLUE, font=FailOver.config["font"]).next_to(dot_http, direction=RIGHT, buff=0.7)
+        dot_dns = Dot(color=self.cfg["dns_color"], radius=DEFAULT_DOT_RADIUS*2).move_to(self.camera.frame.get_left()).shift(RIGHT*1.5+UP*1)
+        dot_http = Dot(color=self.cfg["http_color"], radius=DEFAULT_DOT_RADIUS*2).next_to(dot_dns, direction=DOWN, buff=0.5)
+        dot_dns_text = Text("dns resolution", font_size=24, color=self.cfg["dns_color"], font=self.cfg["font"]).next_to(dot_dns, direction=RIGHT, buff=0.7)
+        dot_http_text = Text("HTTP request", font_size=24, color=self.cfg["http_color"], font=self.cfg["font"]).next_to(dot_http, direction=RIGHT, buff=0.7)
         dot_legend = VGroup(dot_dns, dot_http, dot_dns_text, dot_http_text)
         self.play(FadeIn(happy_tux2, dot_legend), run_time=0.5)
 
@@ -163,18 +181,55 @@ class FailOver(MovingCameraScene):
         dot_r = dot_t.copy().move_to(route)
         dot_c1 = dot_t.copy().move_to(pod11)
         dot_c2 = dot_t.copy().move_to(pod21)
-        self.play(Transform(dot_moving.set_color(RED), dot_r.set_color(RED)))
-        self.play(Transform(dot_moving.set_color(RED), dot_t.set_color(RED)))
+        self.say_scaled("Tux wants to send http request to app.example.com")
+        self.say_scaled("He asks the Route53 about 'app' domain under 'example.com'")
+        self.play(Transform(dot_moving.set_color(self.cfg["dns_color"]), dot_r.set_color(self.cfg["dns_color"])))
+        self.play(Transform(dot_moving.set_color(self.cfg["dns_color"]), dot_t.set_color(self.cfg["dns_color"])))
         self.wait(0.4)
-        self.play(Transform(dot_moving.set_color(BLUE), dot_c1.set_color(BLUE)), run_time=0.5)
-        self.play(Transform(dot_moving.set_color(BLUE), dot_t.set_color(BLUE)), run_time=0.5)
-        self.play(Transform(dot_moving.set_color(BLUE), dot_c1.set_color(BLUE)), run_time=0.5)
-        self.play(Transform(dot_moving.set_color(BLUE), dot_t.set_color(BLUE)), run_time=0.5)
-        self.play(Transform(dot_moving.set_color(BLUE), dot_c1.set_color(BLUE)), run_time=0.5)
-        self.play(Transform(dot_moving.set_color(BLUE), dot_t.set_color(BLUE)), run_time=0.5)
+        self.say_scaled("For the next 30 seconds it's 192.168.0.1")
+        self.play(Transform(dot_moving.set_color(self.cfg["http_color"]), dot_c1.set_color(self.cfg["http_color"])), run_time=0.5)
+        self.play(Transform(dot_moving.set_color(self.cfg["http_color"]), dot_t.set_color(self.cfg["http_color"])), run_time=0.5)
+        self.play(Transform(dot_moving.set_color(self.cfg["http_color"]), dot_c1.set_color(self.cfg["http_color"])), run_time=0.5)
+        self.play(Transform(dot_moving.set_color(self.cfg["http_color"]), dot_t.set_color(self.cfg["http_color"])), run_time=0.5)
+        self.play(Transform(dot_moving.set_color(self.cfg["http_color"]), dot_c1.set_color(self.cfg["http_color"])), run_time=0.5)
+        self.play(Transform(dot_moving.set_color(self.cfg["http_color"]), dot_t.set_color(self.cfg["http_color"])), run_time=0.5)
+        self.say_scaled("Oh no! The pods on cluster in eu went down.")
+
+        # make tux sad
+        sad_tux2 = ImageMobject(fr"images/sad_tux.png")
+        sad_tux2.scale(0.7)
+        sad_tux2.move_to(self.camera.frame.get_left()).shift(RIGHT*1.5+DOWN*3)
+        self.play(Transform(happy_tux2, sad_tux2), run_time=1.5)
+
+        self.say_scaled("Luckily, k8gb controller on cluster 2 kicks in")
+        
+        # todo: display light bulp or shake the icon
+        self.say_scaled("and updates the DNS records to point to cluster 2")
+        # change the records in the bubble
+        dns_bubble_r3 = Text("192.168.1.1 (node 1 @ us)", color=self.cfg["dns_color"], font_size=18, font=self.cfg["font"])
+        dns_bubble_r4 = Text("192.168.1.2 (node 2 @ us)", color=self.cfg["dns_color"], font_size=18, font=self.cfg["font"])
+        dns_bubble_r3.next_to(dns_bubble_title, direction=DOWN, buff=0.4)
+        dns_bubble_r4.next_to(dns_bubble_r1, direction=DOWN, buff=0.2)
+        self.play(Unwrite(dns_bubble_r1), run_time=0.5)
+        self.play(Unwrite(dns_bubble_r2), run_time=0.5)
+        self.play(Write(dns_bubble_r3), run_time=0.5)
+        self.play(Write(dns_bubble_r4), run_time=0.5)
+
+        self.say_scaled("When TTL expires, tux creates another DNS request")
+
+        self.play(Transform(dot_moving.set_color(self.cfg["dns_color"]), dot_r.set_color(self.cfg["dns_color"])))
+        self.play(Transform(dot_moving.set_color(self.cfg["dns_color"]), dot_t.set_color(self.cfg["dns_color"])))
+        self.say_scaled("and starts communicating with the correct cluster")
+        self.play(Transform(dot_moving.set_color(self.cfg["http_color"]), dot_c2.set_color(self.cfg["http_color"])), run_time=0.5)
+        self.play(Transform(dot_moving.set_color(self.cfg["http_color"]), dot_t.set_color(self.cfg["http_color"])), run_time=0.5)
+        # make tux happy
+        self.play(Transform(happy_tux2, happy_tux2_orig), run_time=1.5)
+        self.play(Transform(dot_moving.set_color(self.cfg["http_color"]), dot_c2.set_color(self.cfg["http_color"])), run_time=0.5)
+        self.play(Transform(dot_moving.set_color(self.cfg["http_color"]), dot_t.set_color(self.cfg["http_color"])), run_time=0.5)
+
 
     def say(self, what, ephemeral=True):
-        t = Text(what, color=FailOver.config["font_color"], font_size=23, font=FailOver.config["font"])
+        t = Text(what, color=self.cfg["font_color"], font_size=23, font=self.cfg["font"])
         t.to_edge(DR, buff=0.5)
         self.play(Write(t))
         if ephemeral:
@@ -184,7 +239,7 @@ class FailOver(MovingCameraScene):
             return t
 
     def say_scaled(self, what, ephemeral=True):
-        t = Text(what, color=FailOver.config["font_color"], font_size=46, font=FailOver.config["font"])
+        t = Text(what, color=self.cfg["font_color"], font_size=46, font=self.cfg["font"])
         t.move_to(self.camera.frame.get_right() + self.camera.frame.get_bottom()).shift(UP*1.5+LEFT*(t.width*0.6))
         self.play(Write(t))
         if ephemeral:
@@ -193,5 +248,13 @@ class FailOver(MovingCameraScene):
         else:
             return t
 
-
-
+    def speech_bubble(self):
+        Hexagon = [(0,0,0),
+            (1,-0.1,0),
+            (1,0.7,0),
+            (5,0.7,0),
+            (5,-1.7,0),
+            (1,-1.7,0),
+            (1,-0.55,0),
+        ]
+        return Polygon(*Hexagon, color=self.cfg["dns_color"])
