1d6115fce02368075021152f347ba671096b6854
Jirka Kremser <jiri.kremser@gmail.com>
2021-12-08 15:13:33 +0000

    own gh action - vol4
    Signed-off-by: Jirka Kremser <jiri.kremser@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,9 @@
 
 cd /workspace
 manim $@
-gif=$(find . -name "*.gif")
-echo "::set-output name=gif_path::$video"
+
+tree ./media
+gif=$(find ./media -name "*.gif")
+cp $gif /github/workspace/k8gb.gif
+echo "::set-output name=gif_path::$gif"
+echo "$gif"
