f377df71dd69a384a4f9b519574846aa4bca2725
Jirka Kremser <jiri.kremser@gmail.com>
2021-12-09 12:00:16 +0000

    Render the real scene + fix some issues caused by refactoring
    Signed-off-by: Jirka Kremser <jiri.kremser@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,13 @@
 
 cd /workspace
 manim $@
+echo -e "\n\nResults:"
 tree ./media
 gif=$(find ./media -name "*.gif")
+echo -e "\n\nFound following gif file:"
 ls -lh $gif
 cp $gif /github/workspace/example/k8gb.gif
+echo -e "\n\nIt was copied to:"
 ls -lh /github/workspace/example/k8gb.gif
-ls -lh ./media/videos/k8gb/*/partial_movie_files/FailOver/
+# ls -lh ./media/videos/k8gb/*/partial_movie_files/FailOver/
 echo "::set-output name=gif_path::$gif"
-echo "$gif"
